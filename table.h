6f64ac0babea8a655d0b4200496ce178967c0776
Flamboyant0Anya <so_i_love@126.com>
2017-06-01 20:05:12 +0800

    6/1 20:05

--- table.h
+++ table.h
@@ -13,14 +13,16 @@ public:
     bool primary;//这个属性是否是primary
     bool unique;//这个属性是否是unique
     string indexname;//这个属性的indexname，没有就是"noindex"
+    void defAtt(string attDef);
+    Attribute():primary(false),unique(false),indexname("noindex"){}
 };
 
 class Table
 {
 public:
-    int blocknum;//这个表的文件里一共占多少个block
+    int blocknum;//这个表的文件里一共占多少个block，不到1个算0个
     short primary;//primary的位置，-1代表没有
-    vector<tuples*> record;//一个tuple是指一行的数据内容，这个我得catalog管不着，留给record manager了
+    //vector<tuples*> record;//一个tuple是指一行的数据内容，这个我得catalog管不着，留给record manager了
     vector<Attribute> Attr;//表的属性栏
     string getName() const
     {
@@ -31,7 +33,7 @@ public:
         tablename = tbname;
     }
 
-    Table();
+    Table():primary(-1){}
     Table (string tbname, vector<Attribute> Att){
         tablename = tbname;
         Attr = Att;
