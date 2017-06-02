d4e26be6c467c86d0361baccef68b98b2c57ecb1
Flamboyant0Anya <so_i_love@126.com>
2017-06-02 13:26:41 +0800

    13:26 6/2
    修改细节。

--- table.h
+++ table.h
@@ -8,7 +8,7 @@ class Attribute
 {
 public:
     short type;//三种类型int 1, float 2, char 3
-    short length;//int长度4，float 8，char 长度为定义的大小
+    short length;//只有char才有意义， 长度为定义的大小
     string attrname;
     bool primary;//这个属性是否是primary
     bool unique;//这个属性是否是unique
@@ -23,6 +23,7 @@ public:
     int blocknum;//这个表的文件里一共占多少个block，不到1个算0个
     short primary;//primary的位置，-1代表没有
     //vector<tuples*> record;//一个tuple是指一行的数据内容，这个我得catalog管不着，留给record manager了
+    int Attrnum;
     vector<Attribute> Attr;//表的属性栏
     string getName() const
     {
@@ -33,7 +34,7 @@ public:
         tablename = tbname;
     }
 
-    Table():primary(-1){}
+    Table():primary(-1),Attrnum(0){}
     Table (string tbname, vector<Attribute> Att){
         tablename = tbname;
         Attr = Att;
@@ -44,7 +45,7 @@ private:
 
 };
 //这个表的catalog在文件中应该是：
-//tablename blocknum primary Attrname1 Attrtype1 attrlength1 primary1(bool) unique1(bool) index1(bool) Attrname2 Attrtype2……
+//tablename blocknum attrnum primary Attrname1 Attrtype1 attrlength1 primary1(bool) unique1(bool) index1(bool) Attrname2 Attrtype2……
 
 
 #endif // TABLE_H
