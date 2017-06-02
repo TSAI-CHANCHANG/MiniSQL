22cb5f6862fe5c6aa3c90e070fcb055f150bf2e4
TSAI-CHANCHANG <czz861924@gmail.com>
2017-06-02 10:43:40 +0800

    Update Interpreter.h

--- Interpreter.h
+++ Interpreter.h
@@ -3,4 +3,13 @@
 #include <string>
 using namespace std;
 
+#define ERROR       -1
+#define QUIT_NUMBER -2
+
+#define SELECT       1
+#define CREATE       2
+#define DROP         3
+#define INSERT       4
+#define QUIT         5
+#define EXEC         6
 string getInput();
